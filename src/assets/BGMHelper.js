aa9937d4cd8ba161700324bf5ad3d62d741e92d8
Azimiao <admin@azimiao.com>
2024-12-01 05:31:36 +0800

    feat: add new bgm

--- src/assets/BGMHelper.js
+++ src/assets/BGMHelper.js
@@ -1,16 +1,20 @@
 const importAllBGM = (requireContext) => {
     let bgm = [
         {
-            title: "風を共に舞う気持ち",
+            title: "空-風を共に舞う気持ち",
             url:"https://book.trails-game.com/bgms/sora-fc/風を共に舞う気持ち.mp3",
         },
         {
-            title: "晴れ渡る空に",
+            title: "闪-晴れ渡る空に",
             url:"https://book.trails-game.com/bgms/sen4/In-a-Clear-Sky.mp3",
         },
         {
-            title: "託されたもの",
+            title: "闪-託されたもの",
             url:"https://book.trails-game.com/bgms/sen3/Things-Entrusted.mp3",
+        },
+        {
+            title: "空-仄かに煌く光",
+            url:"https://book.trails-game.com/bgms/sora-3rd/仄かに煌く光.mp3",
         }
     ];
     requireContext.keys().forEach((item,index) => {
